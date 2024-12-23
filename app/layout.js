e216fa824bc61472610238fa2872350fc5c804bb
HippieJim <hippiejim2896@gmail.com>
2024-12-23 14:36:01 -0600

    Add assets Begin setup of landing page

--- app/layout.js
+++ app/layout.js
@@ -18,11 +18,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[url('/rastaBG.png')]`}
       >
-        {children}
+        <div className='min-h-screen w-full bg-black bg-opacity-30'>
+          {children}
+        </div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className='h-screen w-full flex justify-center items-center text-white text-3xl'>
+      <Image
+        className='mix-blend-screen'
+        src='/logo1.jpeg'
+        alt='logo'
+        width={500}
+        height={500}
+      />
+    </div>
+  );
 }
